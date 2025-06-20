494283f2cc329f7b900a1e4becf4bb8ef9e56c54
Vladislav Tulai <vladislav.tulai@gmail.com>
2025-06-20 19:54:44 +0200

    Added initial PDF creation

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import glob
-
 import pandas as pd
+from fpdf import FPDF
+from pathlib import Path
 
+
+output_path = "PDF/"
 files = []
+Path(output_path).mkdir(exist_ok=True)
 
 def read_excel_data(folder_path="invoices"):
     all_files = glob.glob(f"{folder_path}/*.xlsx")
 
     for file in all_files:
         files.append(pd.read_excel(file, sheet_name="Sheet 1"))
+        name, date = extract_invoice_number(file)
+        create_pdf(name, date)
+
+def create_pdf(name, date):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.cell(w=50, h=12, txt=f"Invoice nr. {name}", align="L", ln=1)
+    pdf.cell(w=50, h=12, txt=f"Date {date}", align="L", ln=1)
+    pdf.output(f"{output_path}{name}-{date}.pdf")
+
+def extract_invoice_number(path_to_xlsx_file):
+    file_name = Path(path_to_xlsx_file).stem
+    name, date = file_name.split('-')
+    return name, date
+
+read_excel_data()
+print(files)
